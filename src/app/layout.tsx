897866b1e435f10a7b9a4d5329fd507c5ff2a068
thienmta <thienmta.py@gmail.com>
2024-09-30 10:07:55 +0700

    feat: add speed insight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Head from "next/head";
+import { SpeedInsights } from '@vercel/speed-insights';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -37,6 +38,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
